99c513dde81033c6cfae17cd1699f7f2c7143749
Alireza <therealalireza2@gmail.com>
2024-07-22 22:54:57 -0700

    first commit

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -3,16 +3,17 @@ import { sp } from "utils/replaceNumber";
 import styles from './Main.module.css'
 
 const Main = ({posts}) =>{
+    console.log(posts);
     const BASEURL = import.meta.env.VITE_BASE_URL;
     return(
         <div className={styles.container}>
             {posts?.data.posts.map((post)=>(
                 <div key={post._id} className={styles.card}>
                     <div className={styles.info}>
-                        <p>{post.options.title}</p>
+                        <p>{post.options?.title}</p>
                         <div>
                             <p>{sp(post.amount)}تومان</p>
-                            <span>{post.options.city}</span>
+                            <span>{post.options?.city}</span>
                         </div>
                     </div>
                     <img src={`${BASEURL}${post.images[0]}`} />
